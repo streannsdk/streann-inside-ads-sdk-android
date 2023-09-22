efc2d41d9ae957a9d526fc1c3ea03ec520b26f56
Snezhe <sneze.manojlovska@gmail.com>
2023-09-22 11:24:25 +0200

    Create Campaign and InsideAd model classes and callbacks, add interactivemedia sdk, add insidead library in demo app

---
+++ insidead/src/main/java/com/streann/insidead/callbacks/CampaignCallback.kt
@@ -0,0 +1,9 @@
+package com.streann.insidead.callbacks
+
+import com.streann.insidead.models.InsideAd
+
+interface CampaignCallback {
+    fun onSuccess(insideAd: InsideAd?)
+
+    fun onError(error: String?)
+}
---
+++ insidead/src/main/java/com/streann/insidead/callbacks/InsideAdCallback.kt
@@ -0,0 +1,23 @@
+package com.streann.insidead.callbacks
+
+interface InsideAdCallback {
+
+    fun insideAdReceived()
+
+    fun insideAdBuffering()
+
+    fun insideAdLoaded()
+
+    fun insideAdPlay()
+
+    fun insideAdResume()
+
+    fun insideAdPause()
+
+    fun insideAdStop()
+
+    fun insideAdError(error: String)
+
+    fun insideAdVolumeChanged(level: Float)
+
+}
