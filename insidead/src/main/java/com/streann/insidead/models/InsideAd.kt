c0ccf269901ac448c09ed636a08ae0a9305978fc
Snezhe <sneze.manojlovska@gmail.com>
2023-09-27 17:11:20 +0200

    Update InsideAd model

--- insidead/src/main/java/com/streann/insidead/models/InsideAd.kt
+++ insidead/src/main/java/com/streann/insidead/models/InsideAd.kt
@@ -1,16 +1,12 @@
 package com.streann.insidead.models
 
+import org.json.JSONObject
+
 data class InsideAd(
-    val id: String? = null,
-    var name: String? = null,
-    private var type: String? = null,
-    private var url: String? = null,
-    private var isForAll: Boolean? = null,
-    private var preloadImage: String? = null,
-    private var showInsideAdsInFullScreen: Boolean? = null,
-    var startAfterSeconds: Int = 0,
-    var showCloseButtonAfterSeconds: Int = 0,
-    var durationInSeconds: Int = 0,
-    var clickActionURL: String? = null,
-    var clientIp: String? = null
+    var adId: String? = null,
+    var campaignId: String? = null,
+    var adType: String? = null,
+    var url: String? = null,
+    var placementId: String? = null,
+    var properties: JSONObject? = null
 )
