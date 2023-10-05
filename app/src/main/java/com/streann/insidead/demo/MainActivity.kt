a304c7e4e96c1f3f7241012c0b4bf950238583aa
Snezhe <sneze.manojlovska@gmail.com>
2023-10-05 12:46:08 +0200

    Add AppController class, and call initializeSdk method from it

--- app/src/main/java/com/streann/insidead/demo/MainActivity.kt
+++ app/src/main/java/com/streann/insidead/demo/MainActivity.kt
@@ -19,10 +19,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupInsideAdView() {
         mInsideAdView = findViewById(R.id.insideAdView)
-        mInsideAdView!!.initializeSdk(
-            "559ff7ade4b0d0aff40888dd", bundleId = packageName,
-            appName = getString(R.string.app_name), appVersion = "1.0", userGender = "Female"
-        )
 
         val showInsideAd = findViewById<TextView>(R.id.showInsideAd)
         showInsideAd.setOnClickListener {
