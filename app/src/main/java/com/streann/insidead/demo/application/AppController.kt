61a2881e52c2d61d13a06bc1422cba1ceddfce38
Snezhe <sneze.manojlovska@gmail.com>
2023-10-05 12:46:40 +0200

    Add AppController class

---
+++ app/src/main/java/com/streann/insidead/demo/application/AppController.kt
@@ -0,0 +1,16 @@
+package com.streann.insidead.demo.application
+
+import android.app.Application
+import com.streann.insidead.InsideAdSdk
+
+class AppController : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        InsideAdSdk.initializeSdk(
+            "559ff7ade4b0d0aff40888dd", userGender = "Female"
+        )
+    }
+
+}
