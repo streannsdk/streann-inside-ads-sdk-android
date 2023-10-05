98363b4579577da045f07a11c7d4f1ff729c9ab3
Snezhe <sneze.manojlovska@gmail.com>
2023-10-05 12:45:11 +0200

    Create global InsideAdSdk class, add needed parameters, add initializeSdk method, Helper class, update macros methods for advertising, player width and height, app info

---
+++ insidead/src/main/java/com/streann/insidead/InsideAdSdk.kt
@@ -0,0 +1,38 @@
+package com.streann.insidead
+
+import android.content.SharedPreferences
+
+object InsideAdSdk {
+
+    var apiKey: String = ""
+    var bundleId: String? = ""
+    var appName: String? = ""
+    var appVersion: String? = ""
+    var appDomain: String? = ""
+    var siteUrl: String? = ""
+    var storeUrl: String? = ""
+    var descriptionUrl: String? = ""
+    var userBirthYear: Int? = 0
+    var userGender: String? = ""
+    var adId: String? = ""
+    var adLimitTracking: Int? = 0
+    var playerWidth: Int = 0
+    var playerHeight: Int = 0
+    var appPreferences: SharedPreferences? = null
+
+    fun initializeSdk(
+        apiKey: String, appDomain: String? = "",
+        siteUrl: String? = "", storeUrl: String? = "", descriptionUrl: String? = "",
+        userBirthYear: Int? = 0, userGender: String? = ""
+    ) {
+
+        this.apiKey = apiKey
+        this.appDomain = appDomain
+        this.siteUrl = siteUrl
+        this.storeUrl = storeUrl
+        this.descriptionUrl = descriptionUrl
+        this.userBirthYear = userBirthYear
+        this.userGender = userGender
+    }
+
+}
