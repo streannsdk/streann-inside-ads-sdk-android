bed6a3f1fa9d62b3601f6f05eecc2028239f1dce
Snezhe <sneze.manojlovska@gmail.com>
2024-02-09 15:25:16 +0100

    Update MobileSdk initialization

--- insidead/src/main/java/com/streann/insidead/players/bannerads/BannerAdsPlayer.kt
+++ insidead/src/main/java/com/streann/insidead/players/bannerads/BannerAdsPlayer.kt
@@ -10,7 +10,6 @@ import android.widget.FrameLayout
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.admanager.AdManagerAdRequest
 import com.google.android.gms.ads.admanager.AdManagerAdView
 import com.google.gson.Gson
@@ -38,12 +37,7 @@ class BannerAdsPlayer constructor(
     private var closeBannerAdHandler: Handler? = null
 
     init {
-        init()
-    }
-
-    private fun init() {
         LayoutInflater.from(context).inflate(R.layout.banner_ad_player, this)
-        MobileAds.initialize(context) { }
     }
 
     fun playAd(insideAd: InsideAd, callback: InsideAdCallback) {
